c7adeb3a62bfeee11422780491095f801761c6b4
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-07-09 10:02:14 +0530

    Initial commit: Beat-the-Alarm backend setup

---
+++ chess-alarm/src/main/java/com/chessalarm/ChessAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.chessalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessAlarmApplication.class, args);
+	}
+
+}
---
+++ chess-alarm/src/test/java/com/chessalarm/ChessAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
